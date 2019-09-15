8cd5927185d2da52269c85fb52088a881d834407
Lezzxy <55340035+Lezzxy@users.noreply.github.com>
2019-09-15 15:31:43 +0700

    Add files via upload

---
+++ Perkalian.py
@@ -0,0 +1,12 @@
+import math
+# pip install pyfiglet
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("PERKALIAN")
+print(ascii_banner)
+q = "author = Lezzcod3"
+print(q)
+x = input("angka yang ingin dikali= ")
+y = input("dikali dengan= ")
+
+print("Jumlahnya= ", int(x)*int(y))
